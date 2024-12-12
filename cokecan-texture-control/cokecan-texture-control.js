ae17e45cf5d92dbee9f652c654a1d036db772e2d
Bowen Truelove <botruelove2002@gmail.com>
2024-12-12 13:57:03 -0600

    added files

---
+++ cokecan-texture-control/cokecan-texture-control.js
@@ -0,0 +1,257 @@
+var canvas, gl;
+var program;
+
+var pointsArray = [];
+var textureArray = [];
+var texture1;
+
+// Variables that control the orthographic projection bounds.
+var y_max = 5;
+var y_min = -5;
+var x_max = 8;
+var x_min = -8;
+var near = -50;
+var far = 50;
+
+var modelViewMatrix, projectionMatrix;
+var modelViewMatrixLoc, projectionMatrixLoc;
+
+// namespace contain all the project information
+var AllInfo = {
+
+    // Camera pan control variables.
+    zoomFactor : 8,
+    translateX : 0,
+    translateY : 0,
+
+    // Camera rotate control variables.
+    phi : 1,
+    theta : 0.5,
+    radius : 1,
+    dr : 2.0 * Math.PI/180.0,
+
+    // Mouse control variables
+    mouseDownRight : false,
+    mouseDownLeft : false,
+
+    mousePosOnClickX : 0,
+    mousePosOnClickY : 0
+};
+
+// Form the 4x4 scale transformation matrix
+function scale4(a, b, c) {
+   var result = mat4();
+   result[0][0] = a;
+   result[1][1] = b;
+   result[2][2] = c;
+   return result;
+}
+
+window.onload = function init() {
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //  Load shaders and initialize attribute buffers
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    // Pawn drawn with mesh - surface of revolution
+    var radius = .8;
+    GenSpherePoints(radius);
+
+    var tBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, tBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(textureArray), gl.STATIC_DRAW );
+
+    var vTexCoord = gl.getAttribLocation( program, "vTexCoord" );
+    gl.vertexAttribPointer( vTexCoord, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vTexCoord );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(pointsArray), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
+    // Set the position of the eye
+    document.getElementById("eyeValue").onclick=function() {
+        eyeX=document.parameterForm.xValue.value;
+        eyeY=document.parameterForm.yValue.value;
+        eyeZ=document.parameterForm.zValue.value;
+        render();
+    };
+
+    // These four just set the handlers for the buttons.
+    document.getElementById("thetaup").addEventListener("click", function(e) {
+        AllInfo.theta += AllInfo.dr;
+        render();
+    });
+    document.getElementById("thetadown").addEventListener("click", function(e) {
+        AllInfo.theta -= AllInfo.dr;
+        render();
+    });
+    document.getElementById("phiup").addEventListener("click", function(e) {
+        AllInfo.phi += AllInfo.dr;
+        render();
+    });
+    document.getElementById("phidown").addEventListener("click", function(e) {
+        AllInfo.phi -= AllInfo.dr;
+        render();
+    });
+
+    // Set the scroll wheel to change the zoom factor.
+    // wheelDelta returns an integer value indicating the distance that the mouse wheel rolled.
+    // Negative values mean the mouse wheel rolled down. The returned value is always a multiple of 120.
+    document.getElementById("gl-canvas").addEventListener("wheel", function(e) {
+        if (e.wheelDelta > 0) {
+            AllInfo.zoomFactor = Math.max(0.1, AllInfo.zoomFactor - 0.1);
+        } else {
+            AllInfo.zoomFactor += 0.1;
+        }
+        render();
+    });
+
+    //************************************************************************************
+    //* When you click a mouse button, set it so that only that button is seen as
+    //* pressed in AllInfo. Then set the position. The idea behind this and the mousemove
+    //* event handler's functionality is that each update we see how much the mouse moved
+    //* and adjust the camera value by that amount.
+    //************************************************************************************
+    document.getElementById("gl-canvas").addEventListener("mousedown", function(e) {
+        if (e.which == 1) {
+            AllInfo.mouseDownLeft = true;
+            AllInfo.mouseDownRight = false;
+            AllInfo.mousePosOnClickY = e.y;
+            AllInfo.mousePosOnClickX = e.x;
+        } else if (e.which == 3) {
+            AllInfo.mouseDownRight = true;
+            AllInfo.mouseDownLeft = false;
+            AllInfo.mousePosOnClickY = e.y;
+            AllInfo.mousePosOnClickX = e.x;
+        }
+        render();
+    });
+
+    document.addEventListener("mouseup", function(e) {
+        AllInfo.mouseDownLeft = false;
+        AllInfo.mouseDownRight = false;
+        render();
+    });
+
+    document.addEventListener("mousemove", function(e) {
+        if (AllInfo.mouseDownRight) {
+            AllInfo.translateX += (e.x - AllInfo.mousePosOnClickX)/30;
+            AllInfo.mousePosOnClickX = e.x;
+
+            AllInfo.translateY -= (e.y - AllInfo.mousePosOnClickY)/30;
+            AllInfo.mousePosOnClickY = e.y;
+        } else if (AllInfo.mouseDownLeft) {
+            AllInfo.phi += (e.x - AllInfo.mousePosOnClickX)/100;
+            AllInfo.mousePosOnClickX = e.x;
+
+            AllInfo.theta += (e.y - AllInfo.mousePosOnClickY)/100;
+            AllInfo.mousePosOnClickY = e.y;
+        }
+        render();
+    });
+
+    render();
+}
+
+function loadTexture(texture, whichTexture)
+{
+    // Flip the image's y axis
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+
+    // Enable texture unit passed in as parameter "texture"
+    gl.activeTexture(whichTexture);
+
+    // bind the texture object to the target
+    gl.bindTexture( gl.TEXTURE_2D, texture);
+
+    // set the texture image
+    gl.texImage2D( gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, texture.image );
+
+    // version 1 (combination needed for images that are not powers of 2
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+
+    // set the texture parameters
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri( gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+}
+
+var at = vec3(0, 0, 0);
+var up = vec3(0, 1, 0);
+var eye = vec3(1, 1, 1);
+
+var eyeX=2, eyeY=2, eyeZ=2; // default eye position input values
+
+var render = function() {
+
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Setup the projection matrix.
+    // Study 1) Use a fixed viewing volume
+    // projectionMatrix = ortho(-8, 8, -8, 8, -20, 20);
+    // Study 2) Use a viewing volume changed via mouse movements
+    projectionMatrix = ortho( x_min*AllInfo.zoomFactor - AllInfo.translateX,
+                              x_max*AllInfo.zoomFactor - AllInfo.translateX,
+                              y_min*AllInfo.zoomFactor - AllInfo.translateY,
+                              y_max*AllInfo.zoomFactor - AllInfo.translateY,
+                              near, far);
+    gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix));
+
+    // Setup the initial model-view matrix.
+
+    // study 1) learn the effect of eye position by entering specific eye positions from user interface
+    //eye= vec3(eyeX, eyeY, eyeZ);
+
+    // Study 2) Setup the eye to move around different points on a sphere
+    eye = vec3( AllInfo.radius*Math.cos(AllInfo.phi),
+                AllInfo.radius*Math.sin(AllInfo.theta),
+                AllInfo.radius*Math.sin(AllInfo.phi));
+    /*
+    eye = vec3( AllInfo.radius*Math.cos(AllInfo.phi)*Math.sin(AllInfo.theta),
+                AllInfo.radius*Math.sin(AllInfo.phi)*Math.sin(AllInfo.theta),
+                AllInfo.radius*Math.cos(AllInfo.theta));*/
+
+    modelViewMatrix = lookAt(eye, at, up);
+    var s = scale4(20, 20, 20);
+    var r = rotate(180, 0, 0, 1);
+    modelViewMatrix = mult(s, mult(r, modelViewMatrix));
+    gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix));
+
+    // ========  Establish Textures =================
+    texture1 = gl.createTexture();
+
+    // create the image object
+    texture1.image = new Image();
+
+    // Tell the broswer to load an image
+    texture1.image.src='coke.jpg';
+    //texture1.image.src='whitewindow.jpg';
+    //texture1.image.src='wood.jpg';
+
+    // register the event handler to be called on loading an image
+    texture1.image.onload = function() {  loadTexture(texture1, gl.TEXTURE0); }
+
+    // set the texture unit 0 the sampler
+    gl.uniform1i(gl.getUniformLocation(program, "texture"), 0);
+
+    //gl.drawArrays( gl.TRIANGLES, 0, pointsArray.length);
+    gl.drawArrays( gl.TRIANGLES, 0, pointsArray.length);
+}
